a9fd337e16a3346958793e75968bc7562b5ee34a
Víctor Nunes dos Santos <44742@mpro.mp.br>
2024-08-07 22:39:28 -0400

    refactor: use todo service in appComponent

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { randText } from '@ngneat/falso';
+import { Component, inject, OnInit } from '@angular/core';
+import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
   standalone: true,
@@ -16,36 +17,18 @@ import { randText } from '@ngneat/falso';
   styles: [],
 })
 export class AppComponent implements OnInit {
-  todos!: any[];
-
+  todos!: Todo[];
+  todoService = inject(TodoService);
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
-      .subscribe((todos) => {
-        this.todos = todos;
-      });
+    this.todoService.getAllTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
   }
-
-  update(todo: any) {
-    this.http
-      .put<any>(
-        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-        JSON.stringify({
-          todo: todo.id,
-          title: randText(),
-          body: todo.body,
-          userId: todo.userId,
-        }),
-        {
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        },
-      )
-      .subscribe((todoUpdated: any) => {
-        this.todos[todoUpdated.id - 1] = todoUpdated;
-      });
+  update(todo: Todo) {
+    this.todoService.update(todo).subscribe((todoUpdated: Todo) => {
+      this.todos[todoUpdated.id - 1] = todoUpdated;
+    });
   }
 }
